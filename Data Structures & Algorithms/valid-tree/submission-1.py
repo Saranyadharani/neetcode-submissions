0d9c38e55e7721aef762edb708873872cbe37dd8
Saranya M <saranyadharani84@gmail.com>
2026-09-26 12:04:26 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges)!=n-1:
+            return False
+        # create the union-find ds
+        parent=list(range(n))
+        rank=[0]*n
+        def find(x):
+            if parent[x]!=x:
+                parent[x]=find(parent[x])
+            return parent[x]
+        def union(x,y):
+            px,py=find(x),find(y)
+            if px==py:
+                return False
+            if rank[px]<rank[py]:
+                px,py=py,px
+            parent[py]=px
+            if rank[px]==rank[py]:
+                rank[px]+=1
+            return True
+        for (a,b) in edges:
+            if not union(a,b):
+                return False
+        return True
